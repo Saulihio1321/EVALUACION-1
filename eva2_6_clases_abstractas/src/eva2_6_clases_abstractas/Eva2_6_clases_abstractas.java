594fd6d830aadb7fb3cb4828be82e46a1ea277dd
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2024-03-11 18:49:19 -0600

    Add files via upload

---
+++ eva2_6_clases_abstractas/src/eva2_6_clases_abstractas/Eva2_6_clases_abstractas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_6_clases_abstractas;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva2_6_clases_abstractas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Triangulo trinagulo = new Triangulo();
+        //Figura figura = new figura error no se crean objetos de clases abstractas
+    }
+    
+}
